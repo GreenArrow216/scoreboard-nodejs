9e18300688438820ac80a3bb2874cf68ae0fedad
Green Arrow <gowthamanganesan021@gmail.com>
2024-12-29 15:44:04 +0000

    connected to railway db

--- db.js
+++ db.js
@@ -1,20 +1,19 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'postgres',
-  host: '192.168.0.18',       // Replace with your server IP if remote
-  database: 'scoreboard',  // Replace with your database name
-  password: '2321',          // No password
-  port: 5432,              // Default PostgreSQL port
+  connectionString: process.env.DATABASE_HOST,
+  ssl: {
+    rejectUnauthorized: false, // For self-signed certificates
+  },
 });
 
 // export function query(text, params) { return pool.query(text, params); }
-pool.on('connect', () => {
-  console.log('Connected to the PostgreSQL database.');
+pool.on("connect", () => {
+  console.log("Connected to the PostgreSQL database.");
 });
 
-pool.on('error', (err) => {
-  console.error('Unexpected error on the database connection:', err);
+pool.on("error", (err) => {
+  console.error("Unexpected error on the database connection:", err);
   process.exit(-1);
 });
 
